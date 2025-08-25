34efe51db66ea1ef6f27753ec1da23110c423d2e
Luke Floden <luke.floden@gmail.com>
2025-08-25 16:27:46 -0400

    feat: added example for the context message trimmer I am adding to the chat bot

---
+++ langchain-tutorials/trimmer-example.ts
@@ -0,0 +1,31 @@
+import { SystemMessage, HumanMessage, AIMessage, trimMessages } from "@langchain/core/messages";
+
+const trimmer = trimMessages({
+  maxTokens: 10,
+  strategy: "last",
+  tokenCounter: (msgs) => msgs.length,
+  includeSystem: true,
+  allowPartial: false,
+  startOn: "human",
+});
+
+const messages = [
+  new SystemMessage("you're a good assistant"),
+  new HumanMessage("hi! I'm bob"),
+  new AIMessage("hi!"),
+  new HumanMessage("I like vanilla ice cream"),
+  new AIMessage("nice"),
+  new HumanMessage("whats 2 + 2"),
+  new AIMessage("4"),
+  new HumanMessage("thanks"),
+  new AIMessage("no problem!"),
+  new HumanMessage("having fun?"),
+  new AIMessage("yes!"),
+];
+
+const x = await trimmer.invoke(messages);
+console.log(x);
+
+const y = new HumanMessage("asdf");
+// y.toJSON();
+console.log(y.toJSON());
